496f97646326b2df769f33620d4819024af4e01c
Ulises <ulises.aviles@cetys.edu.mx>
2022-07-19 21:45:13 -0700

    Created cover

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,16 @@
 // NextJS imports
 import Head from "next/head";
 import Image from "next/image";
+import Link from "next/link";
 
 // Styles
 import styles from "../styles/pages/Home.module.css";
 
 // Components
 import Layout from "../components/layout";
+
+// Assets
+import me from "../public/assets/me.jpeg";
 
 // Main react component
 export default function Home() {
@@ -21,7 +25,37 @@ export default function Home() {
       {/* Content */}
       <Layout selectedTabName="HOME">
         <main className={styles.main}>
-          <h1 className={styles.title}>Home</h1>
+          {/* Cover */}
+          <div className={styles.coverContainer}>
+            <div className={styles.coverUpperSectionContainer}>
+              <div className={styles.coverTextContainer}>
+                <h1 className={styles.title}>
+                  I&apos;m Ulises Aviles, <br />
+                  full-stack developer.
+                </h1>
+                <p className={styles.description}>
+                  I am 20 year old full-stack developer with bast experience
+                  with frameworks and tools like NextJS, React native and
+                  firebase that allow me to create entire ecosystems of apps. I
+                  am a computer science student and a lifelong learner.
+                </p>
+              </div>
+              <div className={styles.meImageContainer}>
+                <Image src={me} alt="" className={styles.meImage} />
+              </div>
+            </div>
+            <Link href="/contact">
+              <a className={styles.coverCallToAction}>CONTACT ME!</a>
+            </Link>
+          </div>
+
+          {/* Projects */}
+
+          {/* What I do */}
+
+          {/* Tech */}
+
+          {/* Call to action */}
         </main>
       </Layout>
     </div>

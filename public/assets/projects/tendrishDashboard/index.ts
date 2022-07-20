eb204e59d5ea518e7e0ab74a4049c50ccd7c97e2
Ulises <ulises.aviles@cetys.edu.mx>
2022-07-20 11:50:42 -0700

    Added tendrish assets

---
+++ public/assets/projects/tendrishDashboard/index.ts
@@ -0,0 +1,20 @@
+// Imports
+import agenda from "./Agenda.jpg";
+import createRecipe from "./CreateRecipe.jpg";
+import mockup from "./Mockup.jpg";
+import recipeFinder from "./Recipe finder.jpg";
+import stats from "./RecipeStats.jpg";
+import userFinder from "./UserFinder.jpg";
+
+// Exporter
+const Images = {
+  cover: mockup,
+  features: {
+    agenda,
+    createRecipe,
+    stats,
+    recipeFinder,
+    userFinder,
+  },
+};
+export default Images;

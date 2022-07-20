d46b95e0e64adc91012f249237be82ed0ba715b9
Ulises <ulises.aviles@cetys.edu.mx>
2022-07-20 11:50:14 -0700

    Created technologies type

---
+++ config/technologies.ts
@@ -0,0 +1,163 @@
+// Type
+import { Technology } from "../types/technologies";
+
+// Logos
+import {
+  IoLogoGithub,
+  IoLogoFirebase,
+  IoLogoReact,
+  IoLogoPython,
+  IoLogoHtml5,
+  IoLogoCss3,
+  IoLogoNodejs,
+  IoLogoNpm,
+} from "react-icons/io5";
+import {
+  SiNextdotjs,
+  SiRedux,
+  SiAdobexd,
+  SiAdobephotoshop,
+  SiTypescript,
+  SiJavascript,
+  SiYarn,
+  SiExpo,
+  SiStripe,
+  SiJirasoftware,
+  SiClickup,
+} from "react-icons/si";
+
+// Actual technologies
+export const github: Technology = {
+  logo: IoLogoGithub,
+  name: "Github",
+};
+
+export const firebaseCloudFunctions: Technology = {
+  logo: IoLogoFirebase,
+  name: "Cloud functions",
+};
+
+export const firebaseFirestore: Technology = {
+  logo: IoLogoFirebase,
+  name: "Firestore",
+};
+
+export const reactJS: Technology = {
+  logo: IoLogoReact,
+  name: "ReactJS",
+};
+
+export const reactNative: Technology = {
+  logo: IoLogoReact,
+  name: "React native",
+};
+
+export const nextJS: Technology = {
+  logo: SiNextdotjs,
+  name: "Next.js",
+};
+
+export const redux: Technology = {
+  logo: SiRedux,
+  name: "Redux",
+};
+
+export const adobeXd: Technology = {
+  logo: SiAdobexd,
+  name: "Adobe Xd",
+};
+
+export const adobePhotoshop: Technology = {
+  logo: SiAdobephotoshop,
+  name: "Photoshop",
+};
+
+export const typeScript: Technology = {
+  logo: SiTypescript,
+  name: "TypeScript",
+};
+
+export const javaScript: Technology = {
+  logo: SiJavascript,
+  name: "JavaScript",
+};
+
+export const python: Technology = {
+  logo: IoLogoPython,
+  name: "Python",
+};
+
+export const html: Technology = {
+  logo: IoLogoHtml5,
+  name: "HTML 5",
+};
+
+export const css: Technology = {
+  logo: IoLogoCss3,
+  name: "CSS 3",
+};
+
+export const nodeJS: Technology = {
+  logo: IoLogoNodejs,
+  name: "NodeJS",
+};
+
+export const npm: Technology = {
+  logo: IoLogoNpm,
+  name: "npm",
+};
+
+export const yarn: Technology = {
+  logo: SiYarn,
+  name: "yarn",
+};
+
+export const expo: Technology = {
+  logo: SiExpo,
+  name: "Expo",
+};
+
+export const eas: Technology = {
+  logo: SiExpo,
+  name: "EAS",
+};
+
+export const stripe: Technology = {
+  logo: SiStripe,
+  name: "Stripe",
+};
+
+export const jira: Technology = {
+  logo: SiJirasoftware,
+  name: "Jira",
+};
+
+export const clickUp: Technology = {
+  logo: SiClickup,
+  name: "ClickUp",
+};
+
+export const allTechnologies: Technology[] = [
+  github,
+  firebaseCloudFunctions,
+  firebaseFirestore,
+  reactJS,
+  nextJS,
+  reactNative,
+  expo,
+  eas,
+  typeScript,
+  javaScript,
+  html,
+  css,
+  nodeJS,
+  npm,
+  yarn,
+  redux,
+  adobeXd,
+  adobePhotoshop,
+  python,
+  stripe,
+  jira,
+  clickUp,
+];

27f3b50f27f64d4da715ba0c1466e5431687a540
Ulises <ulises.aviles@cetys.edu.mx>
2022-07-20 20:04:55 -0700

    Added seo related things to file

--- pages/projects/[id].tsx
+++ pages/projects/[id].tsx
@@ -1,6 +1,9 @@
 // NextJS imports
 import Head from "next/head";
-import Image from "next/image";
+
+// Types
+import { Project as ProjectType } from "../../types/projects";
+import { GetStaticPropsContext } from "next";
 
 // Styles
 import styles from "../../styles/pages/Home.module.css";
@@ -8,20 +11,38 @@ import styles from "../../styles/pages/Home.module.css";
 // Components
 import Layout from "../../components/layout";
 
+// Helpers
+import { getProjectsIds, getProjectWithId } from "../../config/helpers";
+
+// SEO related
+export async function getStaticPaths() {
+  const paths = getProjectsIds().map((id) => ({ params: { id } }));
+  return {
+    paths: paths,
+    fallback: true, // false or 'blocking'
+  };
+}
+
+export const getStaticProps = async (context: GetStaticPropsContext) => {
+  const projectId = context.params.id as string;
+  const project = getProjectWithId(projectId);
+  return { props: { project } };
+};
+
 // Main react component
-export default function Project({ id }) {
+export default function Project({ project }: { project: ProjectType }) {
   return (
     <div className={styles.container}>
       {/* Headers */}
       <Head>
-        <title>Ulises Aviles | {id}</title>
+        <title>Ulises Aviles | {project.title}</title>
         <meta name="description" content="Description" />
       </Head>
 
       {/* Content */}
       <Layout selectedTabName="PROJECT">
         <main className={styles.main}>
-          <h1 className={styles.title}>Project: {id}</h1>
+          <h1 className={styles.title}>Project: {project.title}</h1>
         </main>
       </Layout>
     </div>

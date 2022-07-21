9e077a5ad6dffcedeb7d7a84ec9430ddc4bc6a9f
Ulises <ulises.aviles@cetys.edu.mx>
2022-07-20 20:06:08 -0700

    Modified tech type to fit getStatic props

--- types/technologies.ts
+++ types/technologies.ts
@@ -1,6 +1,30 @@
 import { IconType } from "react-icons";
 
+export type TechnologyName =
+  | "Github"
+  | "Cloud functions"
+  | "Firestore"
+  | "ReactJS"
+  | "React native"
+  | "Next.js"
+  | "Redux"
+  | "Adobe Xd"
+  | "Photoshop"
+  | "TypeScript"
+  | "JavaScript"
+  | "Python"
+  | "HTML 5"
+  | "CSS 3"
+  | "NodeJS"
+  | "npm"
+  | "yarn"
+  | "Expo"
+  | "EAS"
+  | "Stripe"
+  | "Jira"
+  | "ClickUp";
+
 export type Technology = {
   logo: IconType;
-  name: string;
+  name: TechnologyName;
 };

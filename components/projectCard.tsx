8c2ca82bf3eca7f6f2b06e6cc4357c0a115ed52e
Ulises <ulises.aviles@cetys.edu.mx>
2022-07-20 20:02:47 -0700

    Created cards

---
+++ components/projectCard.tsx
@@ -0,0 +1,32 @@
+// Styles
+import Image from "next/image";
+import styles from "../styles/components/ProjectCard.module.css";
+
+// Types
+import { Project } from "../types/projects";
+import Button from "./button";
+
+// Actual component
+const ProjectCard = ({ project }: { project: Project }) => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.imageSuperContainer}>
+        <div className={styles.imageContainer}>
+          <Image src={project.cover} alt="" layout="fixed" height={200} />
+        </div>
+      </div>
+      <div className={styles.txtContainer}>
+        <h3 className={styles.title}>{project.title}</h3>
+        <h4 className={styles.category}>{project.category}</h4>
+        <h4 className={styles.type}>{project.type}</h4>
+        <p className={styles.description}>{project.description.short}</p>
+      </div>
+      <div style={{ height: 10 }} />
+      <Button fontSize={15} href={`projects/${project.id}`} txt="Details" />
+      <div style={{ height: 10 }} />
+    </div>
+  );
+};
+
+// Export
+export default ProjectCard;

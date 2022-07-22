56e5a1bb0c07f58f2164b3bda0d14123fc43b500
Ulises <ulises.aviles@cetys.edu.mx>
2022-07-21 20:46:13 -0700

    Added size to component

--- components/projectCardsContainer.tsx
+++ components/projectCardsContainer.tsx
@@ -7,11 +7,23 @@ import { allProjects } from "../config/projects";
 // Styles
 import styles from "../styles/components/ProjectCard.module.css";
 
+// Types
+import { Project, ProjectSize } from "../types/projects";
+
+// Helpers
+import { getProjectsBySize } from "../config/helpers";
+
 // React component
-const ProjectCardsContainer = () => {
+const ProjectCardsContainer = ({ size }: { size?: ProjectSize }) => {
+  // Define projects to return
+  let projects: Project[] = [];
+  if (!size) projects = allProjects;
+  else projects = getProjectsBySize(size);
+
+  // JSX
   return (
     <div className={styles.projectsCardsContainer}>
-      {allProjects.map((project) => (
+      {projects.map((project) => (
         <ProjectCard key={project.title} project={project} />
       ))}
     </div>

19511068d40b1ad26e874b18111bb1d050af4bcf
Ulises <ulises.aviles@cetys.edu.mx>
2022-07-21 20:46:31 -0700

    added getProjectsBySize, cleaned the code

--- config/helpers.ts
+++ config/helpers.ts
@@ -1,11 +1,16 @@
-import { Project } from "../types/projects";
+// Import types
+import { Project, ProjectSize } from "../types/projects";
 import { Technology, TechnologyName } from "../types/technologies";
+
+// Import data
 import { allProjects } from "./projects";
 import { allTechnologies } from "./technologies";
 
+// General
 export const sleep = async (ms: number) =>
   new Promise((resolve) => setTimeout(resolve, ms));
 
+// Projects
 export const getProjectsIds = () => {
   return allProjects.map((project) => project.id);
 };
@@ -18,6 +23,16 @@ export const getProjectWithId = (id: string): Project | null => {
   return null;
 };
 
+export const getProjectsBySize = (size: ProjectSize): Project[] => {
+  let projects: Project[] = [];
+  for (let i = 0; i < allProjects.length; i++) {
+    const project = allProjects[i];
+    if (project.size === size) projects.push(project);
+  }
+  return projects;
+};
+
+// Technologies
 export const getTechnologies = (names: TechnologyName[]) => {
   let arr: Technology[] = [];
   for (let i = 0; i < allTechnologies.length; i++) {

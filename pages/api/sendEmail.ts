5249d67f05a236427c9cffb7826f8094de4b820a
Ulises <ulises.aviles@cetys.edu.mx>
2022-07-23 21:01:20 -0700

    Moved email sending to the front

--- pages/api/sendEmail.ts
+++ pages/api/sendEmail.ts
@@ -1,19 +1,21 @@
+// NextJS imports
 import { NextApiRequest, NextApiResponse } from "next";
 
 // Email sender
 import emailjs from "@emailjs/browser";
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   // req.body: { name: "", company: "", email: "", city: "", message: "", }
 
   // Constants
   const serviceID = process.env.EMAILJS_SERVICE_ID;
   const templateID = process.env.EMAILJS_TEMPLATE_ID;
   const publicKey = process.env.EMAILJS_PUBLIC_KEY;
-
   // Send email
-  emailjs.send(serviceID, templateID, req.body.inputValues, publicKey);
+  await emailjs.send(serviceID, templateID, req.body.inputValues, publicKey);
 
-  // Return
+  // Success
   res.status(200).send("Success");
-}
+};
+
+export default handler;

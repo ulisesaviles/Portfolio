334675a0b049cd0cab79578aa711d0318077f848
Ulises <ulises.aviles@cetys.edu.mx>
2022-07-25 12:31:03 -0700

    Added chomsky images

---
+++ public/assets/projects/chomsky/index.ts
@@ -0,0 +1,22 @@
+// Imports
+import cover from "./cover.jpg";
+import general from "./general.jpg";
+import quiz from "./quiz.jpg";
+import responsiveness from "./responsiveness.jpg";
+import steps from "./steps.jpg";
+import video from "./video.jpg";
+import qr from "./qr.png";
+
+// Exporter
+const Images = {
+  cover,
+  features: {
+    general,
+    quiz,
+    responsiveness,
+    steps,
+    video,
+  },
+  qr,
+};
+export default Images;

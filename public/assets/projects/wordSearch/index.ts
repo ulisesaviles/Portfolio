ac12460763781cc41ef8f01136822192c9fac7ae
Ulises <ulises.aviles@cetys.edu.mx>
2022-07-24 20:11:56 -0700

    Added word search images

---
+++ public/assets/projects/wordSearch/index.ts
@@ -0,0 +1,18 @@
+// Imports
+import colored from "./colored.jpg";
+import hidden from "./hidden.jpg";
+import default_ from "./default.jpg";
+import cover from "./cover.jpg";
+import qr from "./qr.png";
+
+// Exporter
+const Images = {
+  cover,
+  features: {
+    default: default_,
+    hidden,
+    colored,
+  },
+  qr,
+};
+export default Images;

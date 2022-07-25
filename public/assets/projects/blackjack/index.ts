bebae802713e2234dbe02cf5b4ce27258006c39a
Ulises <ulises.aviles@cetys.edu.mx>
2022-07-24 21:09:41 -0700

    Added blackjack images

---
+++ public/assets/projects/blackjack/index.ts
@@ -0,0 +1,28 @@
+// Imports
+import cover from "./cover.png";
+import ammountToBet from "./ammountToBet.jpg";
+import double from "./double.jpg";
+import hit from "./hit.jpg";
+import instrucciones from "./instrucciones.jpg";
+import results from "./results.jpg";
+import stand from "./stand.jpg";
+import surrender from "./surrender.jpg";
+import watch from "./watch.jpg";
+import qr from "./qr.png";
+
+// Exporter
+const Images = {
+  cover,
+  features: {
+    ammountToBet,
+    double,
+    hit,
+    instrucciones,
+    results,
+    stand,
+    surrender,
+    watch,
+  },
+  qr,
+};
+export default Images;

6a927ed84301877cff62f867740cd3876b61fafd
Ulises <ulises.aviles@cetys.edu.mx>
2022-07-24 19:02:10 -0700

    Added go fish images

---
+++ public/assets/projects/goFish/index.ts
@@ -0,0 +1,22 @@
+// Imports
+import cardToThrow from "./cardToThrow.jpg";
+import choosePlay from "./ChoosePlay.jpg";
+import choosePlayers from "./choosePlayers.jpg";
+import results from "./results.jpg";
+import whereToTake from "./whereToTake.jpg";
+import mockup from "./mockup.jpg";
+import qr from "./qr.png";
+
+// Exporter
+const Images = {
+  cover: mockup,
+  features: {
+    cardToThrow,
+    choosePlay,
+    choosePlayers,
+    results,
+    whereToTake,
+  },
+  qr,
+};
+export default Images;

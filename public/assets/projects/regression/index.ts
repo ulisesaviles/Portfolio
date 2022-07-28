b92a54b39f098020d5eb0653bb09c6651b5b2af1
Ulises <ulises.aviles@cetys.edu.mx>
2022-07-27 22:14:19 -0700

    Added regression calculator images

---
+++ public/assets/projects/regression/index.ts
@@ -0,0 +1,22 @@
+// Imports
+import cover from "./cover.jpg";
+import auto from "./auto.jpg";
+import expo from "./expo.jpg";
+import loga from "./loga.jpg";
+import poli from "./poli.jpg";
+import power from "./power.jpg";
+import qr from "./qr.png";
+
+// Exporter
+const Images = {
+  cover,
+  features: {
+    auto,
+    expo,
+    loga,
+    poli,
+    power,
+  },
+  qr,
+};
+export default Images;

7b0ad373705623bba2b847e50e2f4f802deb2af7
Ulises <ulises.aviles@cetys.edu.mx>
2022-07-27 20:23:00 -0700

    Added turfmasters assets

---
+++ public/assets/projects/turfmasters/index.ts
@@ -0,0 +1,28 @@
+// Imports
+import cover from "./cover.jpg";
+import home from "./home.jpg";
+import about from "./about.jpg";
+import catalouge from "./catalouge.jpg";
+import responsiveness from "./responsiveness.jpg";
+import contact from "./contact.jpg";
+import installation from "./installation.jpg";
+import product from "./product.jpg";
+import clients from "./clients.jpg";
+import qr from "./qr.png";
+
+// Exporter
+const Images = {
+  cover,
+  features: {
+    home,
+    about,
+    catalouge,
+    responsiveness,
+    contact,
+    installation,
+    product,
+    clients,
+  },
+  qr,
+};
+export default Images;

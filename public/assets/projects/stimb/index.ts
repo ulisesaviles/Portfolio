2dee3b47e981797d3abfacc478e8dcaf2a62e1f9
Ulises <ulises.aviles@cetys.edu.mx>
2022-07-27 21:27:01 -0700

    Added stimbs Images

---
+++ public/assets/projects/stimb/index.ts
@@ -0,0 +1,30 @@
+// Imports
+import cover from "./cover.jpg";
+import home from "./home.jpg";
+import about from "./about.jpg";
+import galery from "./galery.jpg";
+import responsiveness from "./responsiveness.jpg";
+import contact from "./contact.jpg";
+import modernDesign from "./modernDesign.jpg";
+import products from "./products.jpg";
+import product from "./product.jpg";
+import services from "./services.jpg";
+import qr from "./qr.png";
+
+// Exporter
+const Images = {
+  cover,
+  features: {
+    home,
+    about,
+    galery,
+    responsiveness,
+    contact,
+    modernDesign,
+    product,
+    products,
+    services,
+  },
+  qr,
+};
+export default Images;

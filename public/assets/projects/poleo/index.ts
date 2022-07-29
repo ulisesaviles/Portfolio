fc21d7fd38a2889390ab4c5c16a8c104586938b8
Ulises <ulises.aviles@cetys.edu.mx>
2022-07-28 23:09:24 -0700

    Added qr

--- public/assets/projects/poleo/index.ts
+++ public/assets/projects/poleo/index.ts
@@ -1,13 +1,14 @@
 // Imports
-import cobro from "./cobro.jpg";
-import create from "./create.jpg";
 import home from "./home.jpg";
+import txt from "./txt.jpg";
 import img from "./img.jpg";
 import money from "./money.jpg";
-import more from "./more.jpg";
+import cobro from "./cobro.jpg";
 import myPolls from "./myPolls.jpg";
-import txt from "./txt.jpg";
+import create from "./create.jpg";
+import more from "./more.jpg";
 import cover from "./cover.jpg";
+import qr from "./qr.jpg";
 
 // Exporter
 const Images = {
@@ -22,5 +23,6 @@ const Images = {
     myPolls,
     txt,
   },
+  qr,
 };
 export default Images;

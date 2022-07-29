efa07f8412519f1068cf41f76e88047a1a982356
Ulises <ulises.aviles@cetys.edu.mx>
2022-07-28 19:24:35 -0700

    Live chat app images

---
+++ public/assets/projects/liveChatApp/index.ts
@@ -0,0 +1,32 @@
+// Imports
+import addFriend from "./addFriend.png";
+import call from "./call.png";
+import chat from "./chat.png";
+import chats from "./chats.png";
+import editProfile from "./editProfile.png";
+import friends from "./friends.png";
+import login from "./login.png";
+import profile from "./profile.png";
+import requests from "./requests.png";
+import signUp from "./signUp.png";
+import welcome from "./welcome.png";
+import cover from "./cover.jpg";
+
+// Exporter
+const Images = {
+  cover,
+  features: {
+    addFriend,
+    call,
+    chat,
+    chats,
+    editProfile,
+    friends,
+    login,
+    profile,
+    requests,
+    signUp,
+    welcome,
+  },
+};
+export default Images;

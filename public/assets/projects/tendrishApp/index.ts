2ed7db3fb215bd5c5f2e07e35d34d63878387443
Ulises <ulises.aviles@cetys.edu.mx>
2022-07-28 19:25:21 -0700

    tendrish apps images

---
+++ public/assets/projects/tendrishApp/index.ts
@@ -0,0 +1,40 @@
+// Imports
+import changeRecipes from "./changeRecipes.png";
+import complexSearch from "./complexSearch.png";
+import exchanges from "./exchanges.png";
+import famPlan from "./famPlan.png";
+import grocery from "./grocery.png";
+import home from "./home.png";
+import likedRecipes from "./likedRecipes.png";
+import mealPlan from "./mealPlan.png";
+import more from "./more.png";
+import nutriValues from "./nutriValues.png";
+import profile from "./profile.png";
+import recipe from "./recipe.png";
+import scheduleAppo from "./scheduleAppo.png";
+import settings from "./settings.png";
+import simpleSearch from "./simpleSearch.png";
+import cover from "./cover.jpg";
+
+// Exporter
+const Images = {
+  cover,
+  features: {
+    changeRecipes,
+    complexSearch,
+    exchanges,
+    famPlan,
+    grocery,
+    home,
+    likedRecipes,
+    mealPlan,
+    more,
+    nutriValues,
+    profile,
+    recipe,
+    scheduleAppo,
+    settings,
+    simpleSearch,
+  },
+};
+export default Images;

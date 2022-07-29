63c92313a2d35f33229bc3795b837e9474874c13
Ulises <ulises.aviles@cetys.edu.mx>
2022-07-28 19:24:52 -0700

    Poleos images

---
+++ public/assets/projects/poleo/index.ts
@@ -0,0 +1,26 @@
+// Imports
+import cobro from "./cobro.jpg";
+import create from "./create.jpg";
+import home from "./home.jpg";
+import img from "./img.jpg";
+import money from "./money.jpg";
+import more from "./more.jpg";
+import myPolls from "./myPolls.jpg";
+import txt from "./txt.jpg";
+import cover from "./cover.jpg";
+
+// Exporter
+const Images = {
+  cover,
+  features: {
+    cobro,
+    create,
+    home,
+    img,
+    money,
+    more,
+    myPolls,
+    txt,
+  },
+};
+export default Images;

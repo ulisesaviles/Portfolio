e6d7478ef7fad8be2089668ead58772e9b5d3068
Ulises Aviles <ulisesaviles.dev@gmail.com>
2025-08-08 15:36:39 -0700

    Add images and update index files for 1Up and Maxify projects

---
+++ public/assets/projects/1up/index.ts
@@ -0,0 +1,20 @@
+// Imports
+import website from "./website.png";
+import leadForm from "./lead-form.png";
+import landing from "./landing-pages.png";
+import automation from "./automations.png";
+import qr from "./qrcode_1upsolar.com.png";
+import cover from "./1up cover.png";
+
+// Exporter
+const Images = {
+  cover,
+  features: {
+    website,
+    leadForm,
+    landing,
+    automation,
+  },
+  qr,
+};
+export default Images;
---
+++ public/assets/projects/maxify/index.ts
@@ -0,0 +1,34 @@
+// Imports
+import qr from "./qrcode_www.gomaxify.com.png";
+import cover from "./maxify-cover.png";
+import customerView from "./customerView.png";
+import liveEdit from "./liveEdit.png";
+import maxia from "./maxia.png";
+import proposal from "./proposal.png";
+import proposals from "./proposals.png";
+import responsive from "./responsive.png";
+import settings from "./settings.png";
+import stats from "./stats.png";
+import teams from "./teams.png";
+import website from "./website.png";
+import liveProposal from "./liveProposal.png";
+
+// Exporter
+const Images = {
+  cover,
+  features: {
+    customerView,
+    liveEdit,
+    maxia,
+    proposal,
+    proposals,
+    responsive,
+    settings,
+    stats,
+    teams,
+    website,
+    liveProposal,
+  },
+  qr,
+};
+export default Images;

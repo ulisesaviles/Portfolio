bcb630a4afdff402f73175554adea68d22a917a2
Ulises Aviles <ulisesaviles.dev@gmail.com>
2025-08-08 18:26:45 -0700

    Add Opleta project images and update index file

---
+++ public/assets/projects/opleta/index.ts
@@ -0,0 +1,40 @@
+// Imports
+import qr from "./qrcode_opleta.png";
+import cover from "./Opleta cover.png";
+import aiRoutine from "./aiRoutine.png";
+import chat from "./Chat.png";
+import design from "./Design.jpg";
+import goalCenter from "./Goal center.png";
+import groups from "./Groups.png";
+import home from "./home.png";
+import more from "./More.png";
+import routine from "./Routine.png";
+import search from "./Search.png";
+import signUp from "./Sign up.png";
+import socialMedia from "./Social media.png";
+import workout from "./Workout.png";
+import workouts from "./Workouts.png";
+import workoutSession from "./workoutSesh.png";
+
+// Exporter
+const Images = {
+  cover,
+  features: {
+    aiRoutine,
+    chat,
+    design,
+    goalCenter,
+    groups,
+    home,
+    more,
+    routine,
+    search,
+    signUp,
+    socialMedia,
+    workout,
+    workouts,
+    workoutSession,
+  },
+  qr,
+};
+export default Images;
